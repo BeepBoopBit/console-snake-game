6888a5bdaeb28dce3eeb2be13c7d70d1e41c613a
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-02-12 19:42:29 +0800

    Refactor design

---
+++ Snake.h
@@ -0,0 +1,66 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include "includes.h"
+
+class SnakeBody{
+private:
+    typedef std::string::size_type sType;
+
+public:
+    SnakeBody(sType x, sType y) : _cX(x), _cY(y){}
+    sType _cX, _cY;
+};
+
+class SnakeHead{
+private:
+    typedef std::string::size_type sType;
+
+public:
+    SnakeHead(std::string &str, sType x, sType y, sType width, sType height) : _screen(&str), _cX(x), _cY(y), _screenWidth(width), _screenHeight(height) {}
+    void move(sType x, sType y){
+        // move head
+        std::cout << "Head:\n";
+        std::cout << "cX: " << _cX << '\n';
+        std::cout << "cY: " << _cY << '\n';
+        std::cout << "nX: " << x << '\n' ;
+        std::cout << "nY: " << y << '\n' ;
+
+        sType prevX = _cX, prevY = _cY;
+        moveInScreen(_cX, _cY, x, y, 'P');
+        _cX = x; _cY = y;
+        
+        auto size = _snakeBody.size();
+        for(sType i = 0; i < size; ++i){
+            std::cout << "Body:[" << i << "]\n";
+            sType &cBodyX = _snakeBody[i]->_cX;
+            sType &cBodyY = _snakeBody[i]->_cY;
+            std::cout << "cBodyX: " << cBodyX << '\n';
+            std::cout << "cBodyY: " << cBodyY << '\n';
+            moveInScreen(cBodyX, cBodyY, prevX, prevY, 'O');
+
+            int tempPrevX = prevX, tempPrevY = prevY;
+            prevX = cBodyX; prevY = cBodyY;
+            cBodyX = tempPrevX; cBodyY = tempPrevY;
+
+            std::cout << "nBodyX: " << cBodyX << '\n';
+            std::cout << "nBodyY: " << cBodyY << '\n';
+        }
+    }
+    void attach(SnakeBody *body){
+        _snakeBody.push_back(body);
+    }
+private:
+    void moveInScreen(sType pX, sType pY, sType nX, sType nY, char chr){
+        (*_screen)[nX+1+(_screenWidth*nY)] = chr;
+        (*_screen)[pX+1+(_screenWidth*pY)] = ' ';
+    }
+
+private:
+    std::string *_screen;
+    std::vector<SnakeBody*> _snakeBody;
+    sType _cX, _cY;
+    sType _screenWidth, _screenHeight;
+};
+
+#endif // SNAKE_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "includes.h"
-
-void stuff(){
-    std::cout << "watermelon" << std::endl;
-}
-
+#include "Snake.h"
 
 int main(){
-    std::thread water(stuff);
+    std::string _screen;
+    for(int i = 0; i < 5; ++i){
+        _screen += std::string(10, ' ') + '\n';
+    }
+    SnakeHead head(_screen,4,1,5,5);
+    SnakeBody *body1 = new SnakeBody(3,1);
+    SnakeBody *body2 = new SnakeBody(2,1);
+    head.attach(body1);
+    head.attach(body2);
+    head.move(4,1);
 
     return 0;
 }
