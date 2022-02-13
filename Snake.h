8f16a2b86f4392ae392773fc6e476ba5d60053e8
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-02-13 11:50:13 +0800

    Refactored Design

--- Snake.h
+++ Snake.h
@@ -10,6 +10,8 @@ private:
 public:
     SnakeBody(sType x, sType y) : _cX(x), _cY(y){}
     sType _cX, _cY;
+    // Needs implementing
+    sType _pX, _pY;
 };
 
 class SnakeHead{
@@ -17,37 +19,107 @@ private:
     typedef std::string::size_type sType;
 
 public:
-    SnakeHead(std::string &str, sType x, sType y, sType width, sType height) : _screen(&str), _cX(x), _cY(y), _screenWidth(width), _screenHeight(height) {}
-    void move(sType x, sType y){
-        // move head
-        //std::cout << "Head:\n";
+    SnakeHead(std::string &str, sType x, sType y, sType width, sType height) : _screen(&str), _screenWidth(width), _screenHeight(height) {
+        if(x > 1 && x < _screenWidth){
+            _cX = x;
+            _pX = x-1;
+        }else{
+            std::cout << "OUT OF BOUNCE.\n";
+            std::cout << "When Initializing the position of the SnakeHead, Make sure that it has 1 more space on his left side";
+        }
+        if(y > 0 && y < _screenWidth){
+            _cY = y;
+            _pY = y;
+        }else{
+            std::cout << "OUT OF BOUNCE.\n";
+            std::cout << "When Initializing the position of the SnakeHead, Make sure that it has 1 more space on his left side (for X)";
+        }
         
-        //std::cout << "cX: " << _cX << '\n';
-        //std::cout << "cY: " << _cY << '\n';
-        //std::cout << "nX: " << x << '\n' ;
-        //std::cout << "nY: " << y << '\n' ;
+    }
+public:
+    void moveUp(){
+        move(_cX, _cY-1);
+    }
+    void moveLeft(){
+        move(_cX-1, _cY);
+    }
+    void moveDown(){
+        move(_cX, _cY+1);
+    }
+    void moveRight(){
+        move(_cX+1, _cY);
+    }
+
+    void checkAttachForBody(){
+        auto size = _uSnakeBody.size();
+        int x = 0,
+            y = 0,
+            i = 0;
+        bool isTrue = false;
+        for(; i < size; ++i){
+            int tempX = _uSnakeBody[i]->_cX,
+                tempY = _uSnakeBody[i]->_cY;
+            if(tempX == _cX && tempY == _cY){
+                x = tempX;
+                y = tempY;
+                isTrue = true;
+                break;
+            }
+        }
+        if(isTrue){
+            attach(_uSnakeBody[i]);
+        }
+    }
+private:
+    // changes _cY
+    void move(sType x, sType y){
 
         sType prevX = _cX, prevY = _cY;
+
+        // change the previous position
+        _pX = prevX; _pY = prevY;
+
+        // move the position
         moveInScreen(_cX, _cY, x, y, 'P');
+
+        // update the current position 
         _cX = x; _cY = y;
         
-        auto size = _snakeBody.size();
+        auto size = _aSnakeBody.size();
         for(sType i = 0; i < size; ++i){
-            //std::cout << "Body:[" << i << "]\n";
-            sType &cBodyX = _snakeBody[i]->_cX;
-            sType &cBodyY = _snakeBody[i]->_cY;
-            //std::cout << "cBodyX: " << cBodyX << '\n';
-            //std::cout << "cBodyY: " << cBodyY << '\n';
+            // get the current position of the current body
+            sType &cBodyX = _aSnakeBody[i]->_cX;
+            sType &cBodyY = _aSnakeBody[i]->_cY;
+
+            // move the position of the current body to the previous position of either the head or the body in front of it
             moveInScreen(cBodyX, cBodyY, prevX, prevY, 'O');
+
+            // create a temporary variable for the previous value of 'x' and 'y'
             int tempPrevX = prevX, tempPrevY = prevY;
+
+            // make the previous value of 'x' and 'y' be equal to the current ("previous" since we move it) 'x' and 'y' of the current body
             prevX = cBodyX; prevY = cBodyY;
+
+            // make the value of 'x' and 'y' inside of the current body be equal to the previous value it is at
             cBodyX = tempPrevX; cBodyY = tempPrevY;
-            //std::cout << "nBodyX: " << cBodyX << '\n';
-            //std::cout << "nBodyY: " << cBodyY << '\n';
         }
     }
     void attach(SnakeBody *body){
-        _snakeBody.push_back(body);
+        // TODO: Make sure that when you're going to attach it, it'll be spawned at the end of the tail
+        // change the position relative to the last body position
+        if(_aSnakeBody.empty()){
+            body->_cX = _pX;
+            body->_cY = _pY;
+        }else{
+            auto beforeLast = _aSnakeBody.size() - 1;
+            auto tempX = _aSnakeBody[beforeLast]->_pX, 
+                 tempY = _aSnakeBody[beforeLast]->_pY;
+            body->_cX = tempX;
+            body->_cY = tempY;
+            body->_pX = tempX;
+            body->_pY = tempY;
+        }
+        _aSnakeBody.push_back(body);
     }
 private:
     void moveInScreen(sType pX, sType pY, sType nX, sType nY, char chr){
@@ -57,8 +129,10 @@ private:
 
 private:
     std::string *_screen;
-    std::vector<SnakeBody*> _snakeBody;
+    std::vector<SnakeBody*> _aSnakeBody; // attached
+    std::vector<SnakeBody*> _uSnakeBody; // unattached
     sType _cX, _cY;
+    sType _pX, _pY;
     sType _screenWidth, _screenHeight;
 };
 
--- main.cpp
+++ main.cpp
@@ -7,16 +7,8 @@ int main(){
         _screen += std::string(6, '#') + '\n';
     }
     SnakeHead head(_screen,4,1,6,6);
-    std::cout << _screen << std::endl;
-    std::cout << std::endl;
-    head.move(1,4);
-    std::cout << _screen << std::endl;
-    std::cout << std::endl;
-    //SnakeBody *body1 = new SnakeBody(3,1);
-    //SnakeBody *body2 = new SnakeBody(2,1);
-    //head.attach(body1);
-    //head.attach(body2);
-    //head.move(4,1);
+    SnakeBody *body1 = new SnakeBody(3,1);
+    SnakeBody *body2 = new SnakeBody(2,1);
 
     return 0;
 }
